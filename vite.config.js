82f937f3fdde2cfa499f0619c4102a7a3d62b82f
DevrajPun <devrajpunthapa@gmail.com>
2024-09-27 01:36:01 +0530

    last commit

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "http://localhost:3000",
+        target: "https://notes4you-server.onrender.com",
         changeOrigin: true,
       },
     },
